bcb82a3e8444a13ae422ed122e5baf1ef303a08a
Tatun <10915455+Ta-tun@users.noreply.github.com>
2026-03-13 20:26:17 +0900

    fix: update Dockerfile for Render port

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from datetime import datetime, timezone, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# これを追加：どこからのアクセスも許可する設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # 本番環境では特定のURLに絞るのがナウい（安全）
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 日本時間（JST）の定義
 JST = timezone(timedelta(hours=+9))
@@ -14,9 +23,12 @@ def read_root():
     # UTCから日本時間（JST）に変換
     now_jst = now_utc.astimezone(JST)
 
-    return {
+    data = {
         "message": "Time Conversion API",
         "local_time_jst": now_jst.strftime("%Y-%m-%d %H:%M:%S"),
         "utc_time      ": now_utc.strftime("%Y-%m-%d %H:%M:%S"),
         "unix_timestamp": int(now_utc.timestamp()) # おまけ：システムでよく使う数値形式
     }
+
+    # indent=4 を指定して返す
+    return JSONResponse(content=data, indent=4)
